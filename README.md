fed437a9bdd1c0a586914bb0c21072d0e76c21dc
Chiro <ficosaputro@gmail.com>
2024-06-27 20:09:38 +0700

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# Experiment
-ini adalah repo eksperiment saya untuk mempelajari github, jika kalian mempunyai saran saya sangat menghargai jika anda menyampaikannya. terimakasih!
---
+++ README.py
@@ -0,0 +1,14 @@
+# Experiment
+class Data:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def Data(self):
+        print('nama saya : ', self.name)
+        print('email saya : ', self.email)
+
+
+data = Data('Fico aldi', 'ficosaputro@gmail.com')
+
+data.Data()
